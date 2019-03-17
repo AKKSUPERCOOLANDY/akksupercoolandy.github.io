fc2da0dd78de057d159410846c8c28ef7d806aa2
unknown <andychengmingwu@gmail.com>
2019-03-17 12:57:38 -0700

    This is my first commit for my first website!

---
+++ index.js
@@ -0,0 +1,16 @@
+function addSquares(x,y)
+{
+	return(x^2 + y*y)
+}
+
+alert(addSquares(3,99))
+
+function concat(str1, str2){
+	return(str1 + str2);
+}
+
+function hello(a, b, c)
+{
+	alert(a*b*c)
+}
+alert(hello(1,2,3));
