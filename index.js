7c5c03c768bb55afe82dbf3903efd95cc51f1b32
unknown <andychengmingwu@gmail.com>
2019-03-24 12:04:08 -0700

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-function addSquares(x,y)
-{
-	return(x^2 + y*y)
+alert("Waring: This is not a safe website to visit. We are not responsible for anything bad that happens. Thank you"
+	<div class="alert alert-danger" role="alert">
+  Waring: This is not a safe website to visit. We are not responsible for anything bad that happens. Thank you.
+function validateForm() {
+ 	var missing = []
+	if($("#name").val() == "")
+		missing.push("name")
+	if($("#email").val() == "")
+		missing.push("email")
+	if($("#message").val() == "")
+		missing.push("message")
+	if(missing.length>0) {
+		$("#formerrors").css("color", "red")
+		$("#formerrors").html("You are missing: " + missing)
+	} else {
+		$("#formerrors").css("color", "green")
+		$("#formerrors").html("Thank you for submitting!")
+	}
 }
 
-alert(addSquares(3,99))
-
-function concat(str1, str2){
-	return(str1 + str2);
+function validateForm1() {
+	$("#formerrors").css("color", "red")
+	$("#formerrors").html("You are missing: missed")
 }
 
-function hello(a, b, c)
-{
-	alert(a*b*c)
+
+function validateForm2() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out 2");
+    return false;
+  }
 }
-alert(hello(1,2,3));
